674d8e643913ca6e00ccb7b79fc2e156dcead3bf
Julian-L-Avila <julianlavilam@gmail.com>
2025-02-02 18:53:11 -0500

    Fixed units mistake.

--- ModernPhysics/Electron-Diffraction/Data/processing.jl
+++ ModernPhysics/Electron-Diffraction/Data/processing.jl
@@ -10,8 +10,8 @@ wavelength_σ(U::Float64, U_σ::Float64 = 0.1e3) = U_σ * wavelength(U) * (U + M
 
 process_data(df_in::DataFrame) = DataFrame(
 "U (kV)" => df_in[!, "U (kV)"],
-"r1" => df_in[!, "2r1 (mm)"] ./ 2.0,
-"r2" => df_in[!, "2r2 (mm)"] ./ 2.0,
+"r1" => df_in[!, "2r1 (cm)"] ./ 2.0,
+"r2" => df_in[!, "2r2 (cm)"] ./ 2.0,
 "λ" => wavelength.(df_in[!, "U (kV)"] * 1e3) * 1e12,
 "λσ" => wavelength_σ.(df_in[!, "U (kV)"] * 1e3) * 1e12
 )
@@ -20,8 +20,8 @@ weighted_linear_regression(df::DataFrame, formula) = coef(lm(formula, df, wts = 
 
 format_dataframe_for_output(df::DataFrame) = DataFrame(
 "#U (kV)" => df[!, "U (kV)"],
-"r1 (mm)" => round.(df[!, "r1"], digits = 2),
-"r2 (mm)" => round.(df[!, "r2"], digits = 2),
+"r1 (cm)" => round.(df[!, "r1"], digits = 2),
+"r2 (cm)" => round.(df[!, "r2"], digits = 2),
 "λ (pm)" => round.(df[!, "λ"], digits = 1),
 "λσ (pm)" => round.(df[!, "λσ"], digits = 1)
 )
@@ -31,11 +31,11 @@ main() = begin
 
   coefs_sp = [weighted_linear_regression(df_sp, @formula(r1 ~ λ)), weighted_linear_regression(df_sp, @formula(r2 ~ λ))]
   slopes_sp = vcat(coefs_sp[1][2], coefs_sp[2][2])
-  parameters_d_sp = 2.0 * 65 ./ slopes_sp
+  parameters_d_sp = 2.0 * 6.5 ./ slopes_sp
 
   coefs_li = [weighted_linear_regression(df_li, @formula(r1 ~ λ)), weighted_linear_regression(df_li, @formula(r2 ~ λ))]
   slopes_li = vcat(coefs_li[1][2], coefs_li[2][2])
-  parameters_d_li = 135.0 ./ slopes_li
+  parameters_d_li = 13.5 ./ slopes_li
 
   println("Weighted linear regression for spherical data (r1, r2):", slopes_sp, parameters_d_sp)
   println("Weighted linear regression for linear data (r1, r2):", slopes_li, parameters_d_li)
@@ -59,7 +59,7 @@ main() = begin
 
   parameters_df = DataFrame(
                             "Type" => ["Spherical r1", "Spherical r2", "Linear r1", "Linear r2"],
-                            "d (mm)" => vcat(parameters_d_sp, parameters_d_li)
+                            "d (pm)" => vcat(parameters_d_sp, parameters_d_li)
                            )
   CSV.write("./parameters-d.tsv", parameters_df, delim = '\t')
 end
